b01dec941eaca8da87e8be1f4cbdde25d6f41ecb
mutemip <paulmutemi2030@gmail.com>
2022-06-15 02:57:08 +0300

    basic functions task done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+// saturdayFun("Swim")
+
+const mondayWork  = function (activity = "go to the office"){
+    return`This Monday, I will ${activity}.`;
+}
+// mondayWork("work from home")
+
+const wrapAdjective = function (lop = "*"){
+    return function (state = "special"){
+        return `You are ${lop}${state}${lop}!`;
+    }
+}
